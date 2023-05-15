b0960e44d3dba3e181c8b06131c3ecb7dd1347dc
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-05-15 21:21:05 +0300

    keyframes corr

--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
-import { keyframes } from '@emotion/react';
 
 export const NavBox = styled.div`
   display: flex;
